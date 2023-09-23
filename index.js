629e6f0d1d3edd24e5a05fa183406ccbc3c6ebb4
ahmetbbss <ahmet.buyukbas@georgebrown.ca>
2023-09-23 12:26:03 -0400

    week03 fixed

--- index.js
+++ index.js
@@ -13,34 +13,36 @@ const server = http.createServer((req, res) => {
     if (req.method !== "GET") {
         res.writeHead(405, { "Content-Type": "application/json" });
         res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+        return;
+    } 
+    
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Welcome to Lab Exercise 03</h1>");
+        return;
+    } else if (req.url === "/Employee") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        const allEmployees = EmployeeModule.getAllEmployees();
+        res.end(JSON.stringify(allEmployees));
+        return;
+    } else if (req.url === "/Employee/names") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        const sortedNames = EmployeeModule.getSortedEmployeeNames();
+        res.end(JSON.stringify(sortedNames));
+        return;
+    } else if (req.url === "/Employee/totalsalary") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        const totalSalary = EmployeeModule.getTotalSalary();
+        res.end(JSON.stringify({ total_salary: totalSalary }));
+        return;
     } else {
-        if (req.url === "/") {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end("<h1>Welcome to Lab Exercise 03</h1>");
-        }
-
-        if (req.url === "/Employee") {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            const allEmployees = EmployeeModule.getAllEmployees();
-            res.end(JSON.stringify(allEmployees));
-        }
-
-        if (req.url === "/Employee/names") {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            const sortedNames = EmployeeModule.getSortedEmployeeNames();
-            res.end(JSON.stringify(sortedNames));
-        }
-
-        if (req.url === "/Employee/totalsalary") {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            const totalSalary = EmployeeModule.getTotalSalary();
-            res.end(JSON.stringify({ total_salary: totalSalary }));
-        }
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
     }
-
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(`{"error": "${http.STATUS_CODES[404]}"}`);
 });
+
+
+
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
